e98b7721989c2bbd30b708fed0bbd5852d678098
Gabriel Stachnik <63073744+Celiek@users.noreply.github.com>
2024-10-05 12:09:48 +0200

    Spring security, validation and updates in pom.xml

--- Backend/src/main/java/com/eShop/Backend/config/EShopUserDetailsService.java
+++ Backend/src/main/java/com/eShop/Backend/config/EShopUserDetailsService.java
@@ -2,21 +2,17 @@ package com.eShop.Backend.config;
 
 import com.eShop.Backend.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
-@Configuration
+@Service
 @RequiredArgsConstructor
-public class ApplicationConfig {
-
+public class EShopUserDetailsService implements UserDetailsService {
     private final UserRepository userRepository;
 
-    @Bean
-    public UserDetailsService userDetailsService(){
-        return username -> userRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    public UserDetails loadByUserName(String username) throws UsernameNotFoundException {
+
     }
 }
---
+++ Backend/src/main/java/com/eShop/Backend/constants/ApplicationConstants.java
@@ -0,0 +1,7 @@
+package com.eShop.Backend.constants;
+
+public class ApplicationConstants {
+    public static final String JWT_SECRET_KEY = "JWT_SECRET";
+    public static final String JWT_SECRET_DEFAULT_VALUE = "7e151c1a3339b6a14dcf11185c0b01c239a7744a462481673a970bed54f216e344fa8a7bbfd57931d7ae27d4c19065cf72a88571dd77524b72e1fd4f6fafc2198c0141d9df5eecc1835267e3cbe9a393b166be083249b44280daf55da92288545e8e1e56e368ced4849b26a7a1fb0c70487b8a798636630d97b6bdf70f4b30f8";
+    public static final String JWT_HEADER = "Authorization";
+}
